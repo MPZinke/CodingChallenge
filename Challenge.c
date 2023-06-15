2e127666721f22fc54719233504253e11034deb1
MPZinke <mathew.zinke@gmail.com>
2023-06-15 11:56:46 -0500

    Creates the challenge in Python; cleans up C code

--- Challenge.c
+++ Challenge.c
@@ -9,6 +9,8 @@
 *       - Order: The order of a card in the lexigraphical order.                                                       *
 *       - Rank: The order of a card within a suit.                                                                     *
 *       - Suit: The order of a card based on the four, (♣, ♦, ♥, ♠)                                                    *
+*       - Permutation: The specific ordering of cards' orders.                                                         *
+*       - Message: The base27 string that James Bond reads.                                                            *
 *                                                                                                                      *
 ***********************************************************************************************************************/
 
@@ -21,6 +23,12 @@
 #include <string.h>
 
 
+#define uint240_t uint64_t
+
+
+// ——————————————————————————————————————————————————— CONSTANTS  ——————————————————————————————————————————————————— //
+// —————————————————————————————————————————————————————————————————————————————————————————————————————————————————— //
+
 // ERRORS
 enum ERRORS
 {
@@ -29,10 +37,6 @@ enum ERRORS
 	STRING_TOO_LONG_ERROR,
 	CHARACTER_NOT_FOUND
 };
-
-
-// ——————————————————————————————————————————————————— CONSTANTS  ——————————————————————————————————————————————————— //
-// —————————————————————————————————————————————————————————————————————————————————————————————————————————————————— //
 
 
 // ———————————————————————————————————————————————— CONSTANTS::CARDS ———————————————————————————————————————————————— //
@@ -57,8 +61,10 @@ const char CHARACTER_MAPPINGS[] = " ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const uint8_t CHARACTER_MAPPINGS_LENGTH = sizeof(CHARACTER_MAPPINGS) - 1;
 
 
-// ———————————————————————————————————————————— CARD INTEGER  CONVERSION ———————————————————————————————————————————— //
 // —————————————————————————————————————————————————————————————————————————————————————————————————————————————————— //
+// —————————————————————————————————————————————————————————————————————————————————————————————————————————————————— //
+
+// ——————————————————————————————————————————— CARD INT-STRING CONVERSION ——————————————————————————————————————————— //
 
 uint8_t rank_value(char rank)
 {
@@ -93,7 +99,7 @@ uint8_t suit_value(char* suit_set)
 }
 
 
-uint8_t card_order_func(char* card)
+uint8_t card_order(char* card)
 /*
 SUMMARY: Convert a card string to its order.
 */
@@ -102,7 +108,7 @@ SUMMARY: Convert a card string to its order.
 }
 
 
-void card_string(uint8_t order, char card[4])
+void card_string(uint8_t order, char card[5])
 {
 	card[0] = RANKS[order % RANKS_LENGTH];
 	uint8_t suit = order / RANKS_LENGTH;
@@ -112,132 +118,8 @@ void card_string(uint8_t order, char card[4])
 }
 
 
-void print_card(uint8_t card_order)
-{
-	char card[5] = {0, 0, 0, 0, 0};
-	card_string(card_order, card);
-	printf("%s", card);
-}
-
-
-// ———————————————————————————————————————————— PERMUTATION  ENUMERATION ———————————————————————————————————————————— //
+// ———————————————————————————————————————————— MESSAGE-INDEX CONVERSION ———————————————————————————————————————————— //
 // —————————————————————————————————————————————————————————————————————————————————————————————————————————————————— //
-
-double factorial(uint8_t n)
-{
-	double product = 1.0;
-	for(uint8_t x = n; x > 0; x--)
-	{
-		product *= (double)x;
-	}
-
-	return product;
-}
-
-
-void shift_array(uint8_t remaining_cards_values[DECK_SIZE], uint8_t starting_index)
-{
-	printf("shift_array::starting_index: %hhu\n", starting_index);
-	for(uint8_t x = 0; x < DECK_SIZE; x++)
-	{
-		printf("%2hhu, ", remaining_cards_values[x]);
-	}
-	printf("\n");
-
-	for(uint8_t x = starting_index; x < DECK_SIZE-1; x++)
-	{
-		remaining_cards_values[x] = remaining_cards_values[x+1];
-	}
-
-	for(uint8_t x = 0; x < DECK_SIZE; x++)
-	{
-		printf("%2hhu, ", remaining_cards_values[x]);
-	}
-	printf("\n");
-}
-
-
-void index_to_permutation(double permutation_index, uint8_t cards[])
-/*
-FROM: https://stackoverflow.com/a/34532109
-*/
-{
-	printf("index_to_permutation\n");
-	// uint8_t cards[DECK_SIZE];  // Array of cards' orders, ordered by output.
-
-	uint8_t remaining_cards_orders[DECK_SIZE];  // Sliding array of remaining card orders. Size determined by `x`.
-	for(uint8_t x = 0; x < DECK_SIZE; x++)
-	{
-		remaining_cards_orders[x] = x;
-	}
-
-	for(uint8_t x = 0; x < DECK_SIZE; x++)
-	{
-		double possible_permutation_group_size = factorial(DECK_SIZE - x - 1);
-		uint32_t cards_orders_index = (uint32_t)(permutation_index / possible_permutation_group_size);
-		assert(0 <= cards_orders_index && cards_orders_index < DECK_SIZE - x);
-		printf("permutation_index: %f, possible_permutation_group_size: %f, cards_orders_index: %u\n", permutation_index, possible_permutation_group_size, cards_orders_index);
-
-		uint8_t card_order = remaining_cards_orders[cards_orders_index];
-		cards[x] = card_order;
-
-		shift_array(remaining_cards_orders, cards_orders_index);
-		permutation_index = permutation_index - (floor(permutation_index / possible_permutation_group_size) * possible_permutation_group_size);
-
-		print_card(card_order);
-		printf("\n\n");
-	}
-}
-
-
-// https://stackoverflow.com/questions/12146910/finding-the-lexicographic-index-of-a-permutation-of-a-given-array
-// Determine permutation from each character offset
-double permutation_to_index(char* cards[])
-{
-	// Iterate cards
-	uint8_t card_values[DECK_SIZE];  // Array of cards' values ordered by when received.
-	for(uint8_t x = 0; x < DECK_SIZE; x++)
-	{
-		card_values[x] = card_order_func(cards[x]);
-	}
-
-	double permutation_index = 0.0;
-	for(uint8_t x = 0; x < DECK_SIZE; x++)
-	{
-		double z = 0.0;
-		for(uint8_t y = x+1; y < DECK_SIZE; y++)
-		{
-			if(card_values[y] < card_values[x])
-			{
-				z += 1.0;
-			}
-		}
-
-		permutation_index += z * factorial(DECK_SIZE - x - 1);
-	}
-
-	return permutation_index;
-}
-
-
-// ——————————————————————————————————————————— MESSAGE INTEGER CONVERSION ——————————————————————————————————————————— //
-// —————————————————————————————————————————————————————————————————————————————————————————————————————————————————— //
-
-uint64_t message_length(char* message)
-{
-	uint64_t length = 0;
-	for(uint64_t length = 0; length < 0xFFFFFFFFFFFFFFFF; length++)
-	{
-		if(message[length] == '\0')
-		{
-			return length;
-		}
-	}
-
-	fprintf(stderr, "STRING_TOO_LONG_ERROR\n");
-	exit(STRING_TOO_LONG_ERROR);
-}
-
 
 uint8_t character_index(char character)
 {
@@ -254,99 +136,157 @@ uint8_t character_index(char character)
 }
 
 
-double message_to_unique_int(char* message)
+uint240_t message_to_permutation_index(char* message)
 /*
 SUMMARY: Converts the message to a unique base 27 value.
 */
 {
-	double permutation_index = 0.0;
+	uint240_t permutation_index = 0.0;
 
 	// Iterate through message
 	for(uint32_t message_index = 0; message[message_index] && message_index < 0xFFFFFFFF; message_index++)
 	{
 		char current_character = message[message_index];
 		uint8_t base27_value = character_index(current_character);
-		printf("%c [%2d of %2d]  ", CHARACTER_MAPPINGS[base27_value], base27_value, CHARACTER_MAPPINGS_LENGTH);
-		permutation_index = (double)((double)CHARACTER_MAPPINGS_LENGTH * permutation_index) + (double)base27_value;
-		printf("%f\n", permutation_index);
+		permutation_index = (uint240_t)((uint240_t)CHARACTER_MAPPINGS_LENGTH * permutation_index) + (uint240_t)base27_value;
 	}
 
 	return permutation_index;
 }
 
 
-void unique_int_to_message(double permutation_index, char* message)
+void permutation_index_to_message(uint240_t permutation_index, char* message)
 {
 	uint32_t x = 0;
 	for(; permutation_index > 0.0 && x < 0xFFFFFFFF; x++)
 	{
-		double mod = fmod(permutation_index, (double)CHARACTER_MAPPINGS_LENGTH);
+		uint240_t mod = fmod(permutation_index, (uint240_t)CHARACTER_MAPPINGS_LENGTH);
 		assert(mod < 0xFF);
-		message[x] = CHARACTER_MAPPINGS[(uint8_t)fabs(mod)];
-		printf("%f %% %hhu = %f\n", permutation_index, CHARACTER_MAPPINGS_LENGTH, mod);
+		message[x] = CHARACTER_MAPPINGS[mod];
 		permutation_index = (permutation_index - mod) / CHARACTER_MAPPINGS_LENGTH;
 	}
 	message[x--] = 0;
 
-	// for(uint32_t left_index = 0; left_index < (x+1) / 2; left_index++)
-	// {
-	// 	uint32_t right_index = x - left_index;
-	// 	message[left_index] ^= message[right_index];
-	// 	message[right_index] ^= message[left_index];
-	// 	message[left_index] ^= message[right_index];
-	// }
+	// String is reversed; unreverse it
+	for(uint32_t left_index = 0; left_index < (x+1) / 2; left_index++)
+	{
+		uint32_t right_index = x - left_index;
+		message[left_index] ^= message[right_index];
+		message[right_index] ^= message[left_index];
+		message[left_index] ^= message[right_index];
+	}
 }
 
+
+// —————————————————————————————————————————— INDEX-PERMUTATION CONVERSION —————————————————————————————————————————— //
+// —————————————————————————————————————————————————————————————————————————————————————————————————————————————————— //
+
+uint240_t factorial(uint8_t n)
+{
+	uint240_t product = 1.0;
+	for(uint8_t x = n; x > 0; x--)
+	{
+		product *= (uint240_t)x;
+	}
+
+	return product;
+}
+
+
+void shift_array(uint8_t remaining_cards_values[DECK_SIZE], uint8_t starting_index)
+{
+	for(uint8_t x = starting_index; x < DECK_SIZE-1; x++)
+	{
+		remaining_cards_values[x] = remaining_cards_values[x+1];
+	}
+}
+
+
+void index_to_permutation(uint240_t permutation_index, uint8_t card_orders[])
+/*
+FROM: https://stackoverflow.com/a/34532109
+*/
+{
+	uint8_t unchosen_cards_orders[DECK_SIZE];  // Sliding array of remaining card orders. Size determined by `x`.
+	for(uint8_t x = 0; x < DECK_SIZE; x++)
+	{
+		unchosen_cards_orders[x] = x;
+	}
+
+	for(uint8_t x = 0; x < DECK_SIZE; x++)
+	{
+		uint240_t permutation_group_size = factorial(DECK_SIZE - x - 1);
+		uint32_t unchosen_cards_orders_index = (uint32_t)(permutation_index / permutation_group_size);
+		assert(0 <= unchosen_cards_orders_index && unchosen_cards_orders_index < DECK_SIZE - x);
+
+		uint8_t card_order = unchosen_cards_orders[unchosen_cards_orders_index];
+		card_orders[x] = card_order;
+
+		shift_array(unchosen_cards_orders, unchosen_cards_orders_index);
+		permutation_index = permutation_index - (floor(permutation_index / permutation_group_size) * permutation_group_size);
+	}
+}
+
+
+uint240_t permutation_to_index(uint8_t card_orders[])
+/*
+https://stackoverflow.com/a/12147386
+*/
+{
+	uint240_t permutation_index = 0.0;
+	for(uint8_t x = 0; x < DECK_SIZE; x++)
+	{
+		uint240_t z = 0.0;
+		for(uint8_t y = x+1; y < DECK_SIZE; y++)
+		{
+			if(card_orders[y] < card_orders[x])
+			{
+				z += 1.0;
+			}
+		}
+
+		permutation_index += z * factorial(DECK_SIZE - x - 1);
+	}
+
+	return permutation_index;
+}
+
+
+void decode(char* cards[], char* message)
+{
+	uint8_t card_orders[DECK_SIZE];
+	for(uint8_t x = 0; x < DECK_SIZE; x++)
+	{
+		card_orders[x] = card_order(cards[x]);
+	}
+
+	uint240_t permutation_index = permutation_to_index(card_orders);
+
+	permutation_index_to_message(permutation_index, message);
+}
+
+
+void encode(char* message, char* cards[])
+{
+	uint240_t permutation_index = message_to_permutation_index(message);
+
+	uint8_t card_orders[DECK_SIZE];
+	index_to_permutation(permutation_index, card_orders);
+
+	for(uint8_t x = 0; x < DECK_SIZE; x++)
+	{
+		card_string(card_orders[x], cards[x]);
+	}
+}
+
+
+// —————————————————————————————————————————————————————— MAIN —————————————————————————————————————————————————————— //
 
 int main()
 {
 	char* cards[] = {"A♣", "2♣", "3♣", "4♣", "5♣", "6♣", "7♣", "8♣", "9♣", "T♣", "J♣", "Q♣", "K♣", "A♦", "2♦", "3♦",
 	  "4♦", "5♦", "6♦", "8♦", "K♠", "A♥", "Q♥", "J♥", "T♦", "T♠", "K♥", "7♠", "9♠", "6♠", "9♥", "2♠", "7♦", "7♥", "6♥",
 	  "Q♦", "8♠", "3♥", "K♦", "5♥", "Q♠", "T♥", "A♠", "5♠", "J♠", "2♥", "4♥", "3♠", "4♠", "J♦", "8♥", "9♦"};
-	// for(uint8_t x = 0; x < DECK_SIZE; x++)
-	// {
-	// 	uint8_t card = card_order_func(cards[x]);
-	// 	printf("%s\n", cards[x]);
-	// 	printf(" %hhu\n", card);
-	// 	card_string(card, cards[x]);
-
-	// 	printf("%s\n", cards[x]);
-	// }
-
-	char message[] = "WAYNEWANTEDTHISTOBEHARDER";
-	double permutation_index1 = message_to_unique_int(message);
-	// double permutation_index1 = permutation_to_index(cards);
-	char message_result[512];
-	message_result[500] = 0;
-	printf("%s\n", message_result);
-	unique_int_to_message(permutation_index1, message_result);
-	printf("%s\n", message_result);
-	// uint8_t cards_result[DECK_SIZE];
-	// index_to_permutation(permutation_index1, cards_result);
-	// for(uint8_t x = 0; x < DECK_SIZE; x++)
-	// {
-	// 	// print_card(cards_result[x]);
-	// 	// printf("\n");
-	// 	char card[5];
-	// 	card_string(cards_result[x], card);
-	// 	if(strcmp(cards[x], card) == 0)
-	// 	{
-	// 		printf("True\n");
-	// 	}
-	// 	else printf("False\n");
-	// }
-
-	// char message[512];
-	// unique_int_to_message(permutation_index1, message);
-
-
-	// double permutation_index2 = message_to_unique_int("WAYNEWANTEDTHISTOBEHARDER");
-
-	// uint8_t cards2[DECK_SIZE];
-	// index_to_permutation((double)permutation_index2, cards2);
-	// printf("%f\n", permutation_index1);
-	// printf("%f\n", permutation_index2);
-
 
 	return 0;
 }
---
+++ Challenge.py
@@ -0,0 +1,141 @@
+#!/opt/homebrew/bin/python3
+# -*- coding: utf-8 -*-
+__author__ = "MPZinke"
+
+########################################################################################################################
+#                                                                                                                      #
+#   created by: MPZinke                                                                                                #
+#   on 2023.06.15                                                                                                      #
+#                                                                                                                      #
+#   DESCRIPTION:                                                                                                       #
+#       - Index: The index at which a permutation is found in lexigraphical order.                                     #
+#       - Order: The order of a card in the lexigraphical order.                                                       #
+#       - Rank: The order of a card within a suit.                                                                     #
+#       - Suit: The order of a card based on the four, (♣, ♦, ♥, ♠)                                                    #
+#       - Permutation: The specific ordering of cards' orders.                                                         #
+#       - Message: The base27 string that James Bond reads.                                                            #
+#                                                                                                                      #
+########################################################################################################################
+
+
+# Playing Cards
+SUITS = "♣♦♥♠"
+RANKS = "A23456789TJQK"
+
+DECK_SIZE = len(SUITS) * len(RANKS)
+
+# Message
+CHARACTER_MAPPINGS = " ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+
+# ———————————————————————————————————————————— CARD INT-STRING CONVERSION ———————————————————————————————————————————— #
+
+def card_order(card_string: str) -> int:
+	return RANKS.index(card_string[0]) + len(RANKS) * SUITS.index(card_string[1])
+
+
+def card_string(card_order: int) -> str:
+	return f"{RANKS[card_order % len(RANKS)]}{SUITS[card_order // len(RANKS)]}"
+
+
+# ————————————————————————————————————————————— MESSAGE-INDEX CONVERSION ————————————————————————————————————————————— #
+
+def message_to_permutation_index(message: str) -> int:
+	permutation_index = 0
+
+	for x, character in enumerate(message):
+		character_value = CHARACTER_MAPPINGS.index(character)
+		permutation_index = permutation_index * len(CHARACTER_MAPPINGS) + character_value
+
+	return permutation_index
+
+
+def permutation_index_to_message(permutation_index: int) -> str:
+	message = ""
+
+	for x in range(0xFFFFFFFF):
+		if(permutation_index == 0):
+			break
+
+		character = CHARACTER_MAPPINGS[permutation_index % len(CHARACTER_MAPPINGS)]
+		message += character
+		permutation_index //= len(CHARACTER_MAPPINGS)
+
+	return message[::-1]
+
+
+# ——————————————————————————————————————————— INDEX-PERMUTATION CONVERSION ——————————————————————————————————————————— #
+
+def factorial(n: int) -> int:
+	product = 1
+	for x in range(1, n+1):
+		product *= x
+
+	return product
+
+
+def index_to_permutation(permutation_index: int) -> list[int]:
+	"""
+	FROM: https://stackoverflow.com/a/34532109
+	"""
+	card_orders = []
+
+	unchosen_cards_orders = [x for x in range(DECK_SIZE)]
+	for x in range(DECK_SIZE):
+		# Number of permutations index falls within based on currently term of n for permutation
+		permutation_group_size = factorial(DECK_SIZE - x - 1)
+		# Index of card order in unchosen card list
+		unchosen_cards_orders_index = permutation_index // permutation_group_size
+		card_orders.append(unchosen_cards_orders[unchosen_cards_orders_index])
+
+		unchosen_cards_orders.pop(unchosen_cards_orders_index)  # remove chosen card
+
+		permutation_index = permutation_index % permutation_group_size  # Continue with remaining permutations
+
+	return card_orders
+
+
+def permutation_to_index(card_orders: list[int]) -> int:
+	"""
+	FROM: https://stackoverflow.com/a/12147386
+	"""
+	permutation_index = 0
+	for x, card_order in enumerate(card_orders):
+		term_multiplier = 0
+		# Determine offset of term to multiply by possibilities
+		for y in range(x+1, DECK_SIZE):
+			if(card_orders[y] < card_order):
+				term_multiplier += 1
+
+		permutation_index += term_multiplier * factorial(DECK_SIZE - x - 1)
+
+	return permutation_index
+
+
+# ——————————————————————————————————————————— INDEX-PERMUTATION CONVERSION ——————————————————————————————————————————— #
+
+def decode(cards: list[str]) -> str:
+	card_orders = [card_order(card) for card in cards]
+	permutation_index = permutation_to_index(card_orders)
+	message = permutation_index_to_message(permutation_index)
+	return message
+
+
+def encode(message: str) -> list[str]:
+	permutation_index = message_to_permutation_index(message)
+	card_orders = index_to_permutation(permutation_index)
+	cards = [card_string(card_order) for card_order in card_orders]
+	return cards
+
+
+def main():
+	cards_input = ['K♣', '7♣', '3♣', 'T♦', '2♣', '9♥', '4♦', 'Q♠', '8♥', 'A♥', 'K♥', '8♠', 'T♠', '4♠', '2♠', '5♦', '9♦', '4♥', '5♥', 'A♣', '7♠', 'J♦', '5♣', '2♥', 'Q♣', 'T♥', 'J♠', '6♠', '6♣', '6♦', 'Q♥', '6♥', '9♠', '3♦', '5♠', '7♥', '2♦', '8♦', 'K♦', 'A♠', '4♣', '3♥', 'A♦', 'J♥', '8♣', 'K♠', 'J♣', '9♣', 'T♣', '3♠', '7♦', 'Q♦']
+	message = decode(cards_input)
+	print(message)
+	cards_output = encode(message)
+	print(cards_output)
+	assert(cards_input == cards_output)
+
+
+if(__name__ == "__main__"):
+	main()
