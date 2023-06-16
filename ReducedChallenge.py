86df6bb189c887331fec8cef61f5c46ef5e9f9dd
MPZinke <mathew.zinke@gmail.com>
2023-06-16 15:27:26 -0500

    Reduces further

--- ReducedChallenge.py
+++ ReducedChallenge.py
@@ -12,9 +12,8 @@ def decode(_):
  a,b,c=[B.index(a[0])+13*C.index(a[1])for a in _],0,""
  for x,d in enumerate(a):
   b+=sum(1 for y in E(x+1,52)if a[y]<d)*D(51-x)
- for x in E(99):
-  if b:
-   c+=A[b%27]
-   b//=27
+ while b:
+  c+=A[b%27]
+  b//=27
  return c[::-1]
 encode=lambda _:[B[a%13]+C[a//13]for a in F(_)]
