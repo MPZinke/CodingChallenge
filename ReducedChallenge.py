e0c1480a421238160550ba3cf77252b2d2ab2db7
MPZinke <mathew.zinke@gmail.com>
2023-06-16 15:20:57 -0500

    Reduces further

--- ReducedChallenge.py
+++ ReducedChallenge.py
@@ -4,19 +4,17 @@ def F(_):
  a,b,c=0,[],[x for x in E(52)]
  for x in _:a=a*27+A.index(x)
  for x in E(52):
-  d=D(52-x-1)
+  d=D(51-x)
   b+=[c.pop(a//d)]
   a%=d
  return b
-def G(_):
- a,b=0,""
- for x,c in enumerate(_):
-  d=sum(1 for y in E(x+1,52)if _[y]<c)
-  a+=d*D(52-x-1)
+def decode(_):
+ a,b,c=[B.index(a[0])+13*C.index(a[1])for a in _],0,""
+ for x,d in enumerate(a):
+  b+=sum(1 for y in E(x+1,52)if a[y]<d)*D(51-x)
  for x in E(99):
-  if a==0:break
-  b+=A[a%27]
-  a//=27
- return b[::-1]
-decode=lambda _:G([B.index(a[0])+13*C.index(a[1])for a in _])
+  if b:
+   c+=A[b%27]
+   b//=27
+ return c[::-1]
 encode=lambda _:[B[a%13]+C[a//13]for a in F(_)]
