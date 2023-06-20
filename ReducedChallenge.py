d59d90947b5384aa3c015be7f90e73ce937caeb9
MPZinke <mathew.zinke@gmail.com>
2023-06-20 13:04:06 -0500

    Further reduces code

--- ReducedChallenge.py
+++ ReducedChallenge.py
@@ -1,12 +1,12 @@
 import math
-A,B,C,D,E=" ABCDEFGHIJKLMNOPQRSTUVWXYZ","A23456789TJQK","CDHS",math.factorial,range
+A,B,C,D=" ABCDEFGHIJKLMNOPQRSTUVWXYZ",[a+b for b in"CDHS"for a in"A23456789TJQK"],math.factorial,range
 def encode(_):
- a,b,c=0,[],list(E(52))
+ a,b,c=0,[],list(D(52))
  for x in _:a=a*27+A.find(x)
- for x in E(52):d=D(51-x);b+=[B[(e:=c.pop(a//d))%13]+C[e//13]];a%=d
+ for x in D(52):d=C(51-x);b+=[B[c.pop(a//d)]];a%=d
  return b
 def decode(_):
- a,b,c=[B.find(a[0])+13*C.find(a[1])for a in _],0,""
- for x,d in enumerate(a):b+=sum(1 for y in E(x+1,52)if a[y]<d)*D(51-x)
+ a,b,c=[B.index(a)for a in _],0,""
+ for x,d in enumerate(a):b+=sum(1 for y in D(x+1,52)if a[y]<d)*C(51-x)
  while b:c=A[b%27]+c;b//=27
  return c
