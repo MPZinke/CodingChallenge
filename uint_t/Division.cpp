78f82cd0159d7bcbf4db2602c649a6d3e52b9dba
mpzinke <mathew.zinke@gmail.com>
2023-06-29 19:30:07 -0500

    Implements arbitary left bit shifting function

--- uint_t/Division.cpp
+++ uint_t/Division.cpp
@@ -26,21 +26,32 @@ PROTOTYPE: `static digit v_lshift(digit *z, digit *a, Py_ssize_t m, int d)`
            `a` — Source
            `m` — Array size
            `d` — shift_amount
+DETAILS:
+         Divide the bits into where they land across digits
+         EG. Bits:     0 1 2 3 4 5 6 7 | 0 1 2 3 4 5 6 7 | 0 1 2 3 4 5 6 7
+                       ----------------|-----------------|----------------
+             Used as:  - - 0 1 2 3 4 5 | - - 0 1 2 3 4 5 | - - 0 1 2 3 4 5
+             Shifted:  - - 3 4 5 0 1 2 | - - 3 4 5 0 1 2 | - - 3 4 5 0 x x
+         Bits 0, 1, 2 are moved left of the split and 3, 4, 5 remain right of the split.
 FROM: https://github.com/python/cpython/blob/d32e8d6070057eb7ad0eb2f9d9f1efab38b2cff4/Objects/longobject.c#L1564
 */
+
 {
-    assert(0 <= shift_amount && shift_amount < DIGIT_SHIFT);
+	uint_t shifted_uint_t(this->_size);
 
-	for(int x = 0; x < size; x++)
+	size_t right_bits_shift = shift_amount % DIGIT_SHIFT;  // shift of bits that will move/stay right of the digit split
+	size_t left_bits_shift = DIGIT_SHIFT - right_bits_shift;  // shift of bits that will move left of the digit split
+	size_t digit_shift = shift_amount / DIGIT_SHIFT;  // number of full digits a digit will move over
+
+	size_t iterations = _size - digit_shift;
+	for(size_t x = 0; x < iterations; x++)
 	{
-		destination[x] |= (source[x] << shift_amount) & MASK;
-		if(x < size-1)
-		{
-			destination[x+1] = source[x] >> (SHIFT - shift_amount);
-		}
+		destination[x+digit_shift] |= (source[x] << right_bits_shift) & MASK;  // move the bits that stay right
+		destination[x+digit_shift+1] = source[x] >> left_bits_shift;
 	}
+	destination[iterations+digit_shift] |= source[iterations] >> left_bits_shift;
 
-	return (source[size-1] << shift_amount) & MASK;
+	return (source[_size-1] << shift_amount) & MASK;
 }
 
 
